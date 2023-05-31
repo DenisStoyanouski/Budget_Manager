4da52afc7a7789331cf7f77e175b0ed9b8110692
Denis Stoyanouski <vandenst@tut.by>
2023-05-31 22:23:53 +0300

    added methods to BudgetManager class

--- src/budget/BudgetManager.java
+++ src/budget/BudgetManager.java
@@ -1,14 +1,12 @@
 package budget;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class BudgetManager {
     private final static Scanner scanner = new Scanner(System.in);
-
+    private final static String currency = "$";
     private final static List<Record> records = new ArrayList<>();
 
     public static void start() {
@@ -17,27 +15,42 @@ public class BudgetManager {
 
     private static void readRecords() {
         while (scanner.hasNextLine()) {
-            records.add(createRecord(getInput()));
+            Record record = createRecord(getInput());
+            if (!record.item.isEmpty()) {
+                records.add(record);
+            }
         }
-        System.out.println("Total:" + getCurrency() + getBalance());
+        printTotal();
     }
 
     private static Record createRecord(String input) {
-        // TODO: 31.05.2023 add process input
-        return new Record("Item", "$", 56);
+        int currencyIndex = input.indexOf('$');
+        try {
+            String item = input.substring(0, currencyIndex);
+            double cost = Double.parseDouble(input.substring(currencyIndex + 1));
+            return new Record(item, currency, cost);
+        } catch (NumberFormatException e) {
+            System.out.println("Record: " + input + " has wrong currency format");
+        }
+        return new Record("", "", 0);
     }
 
-    private static int getBalance() {
-        return records.stream().mapToInt(record -> record.cost).sum();
+    private static String getBalance() {
+        return String.format("%.2f", records.stream().mapToDouble(record -> record.cost).sum());
     }
 
     private static String getCurrency() {
-        return records.get(0).currency;
+        return currency;
     }
 
     private static String getInput() {
         return scanner.nextLine();
     }
 
-    public record Record (String item, String currency, int cost) {}
+    private static void printTotal() {
+        System.out.println("Total: " + getCurrency() + "" + getBalance());
+    }
+
+    public record Record(String item, String currency, double cost) {
+    }
 }
